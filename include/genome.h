5ccd4cdbcf0261e7808ed66dbc4d75b60b209ad6
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-01-19 12:45:29 +0900

    rename gene.h to genome.h

--- include/genome.h
+++ include/genome.h
@@ -1,5 +1,5 @@
-#ifndef _GENE_H
-#define _GENE_H
+#ifndef _GENOME_H
+#define _GENOMEGENE_H
 
 #include <cstdint>
 #include <bitset>
@@ -8,29 +8,29 @@
 #include "rand_gen.h"
 
 template <size_t _Len>
-class gene {
+class genome {
 public:
-	gene() noexcept;
+	genome() noexcept;
 	double eval(std::function<double(std::bitset<_Len>)> env) const;
 	void reset() noexcept;
-	gene<_Len> copy() const noexcept;
+	genome<_Len> copy() const noexcept;
 	constexpr size_t length() const noexcept;
 private:
 	std::bitset<_Len> dna;
 };
 
 template <size_t _Len>
-inline gene<_Len>::gene() noexcept {
+inline genome<_Len>::genome() noexcept {
 	reset();
 }
 
 template <size_t _Len>
-inline double gene<_Len>::eval(std::function<double(std::bitset<_Len>)> env) const {
+inline double genome<_Len>::eval(std::function<double(std::bitset<_Len>)> env) const {
 	return env(dna);
 }
 
 template <size_t _Len>
-inline void gene<_Len>::reset() noexcept {
+inline void genome<_Len>::reset() noexcept {
 	for(int i = 0; i < _Len / 8 + !(!(_Len % 8)); i++) {
 		uint8_t r = static_cast<uint8_t>(rand_gen::getrand<int>());
 		dna <<= 8;
@@ -39,14 +39,14 @@ inline void gene<_Len>::reset() noexcept {
 }
 
 template <size_t _Len>
-inline gene<_Len> gene<_Len>::copy() const noexcept {
-	gene<_Len> g;
+inline genome<_Len> genome<_Len>::copy() const noexcept {
+	genome<_Len> g;
 	g.dna = dna;
 	return dna;
 }
 
 template <size_t _Len>
-constexpr inline size_t gene<_Len>::length() const noexcept {
+constexpr inline size_t genome<_Len>::length() const noexcept {
 	return _Len;
 }
 
